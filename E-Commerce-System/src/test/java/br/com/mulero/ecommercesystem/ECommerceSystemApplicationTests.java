cf525ecd9e410a443d3db8be3bd58eba2eb2297c
Richard Mulero <mulerolp@gmail.com>
2024-06-17 23:10:50 -0300

    feat: criando projetos bases

---
+++ E-Commerce-System/src/test/java/br/com/mulero/ecommercesystem/ECommerceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mulero.ecommercesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Inventory-Management/src/test/java/br/com/mulero/inventorymanagement/InventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mulero.inventorymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
